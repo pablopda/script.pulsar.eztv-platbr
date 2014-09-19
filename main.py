96bd48eac6d2b8aefed1aa404a33e6ec7ff32990
Wagner Caixeta <wagner.caixeta@gmail.com>
2014-09-19 12:44:19 -0300

    Version 0.0.4 Better configiguration. Better cache system.

--- main.py
+++ main.py
@@ -6,22 +6,28 @@ import urllib2
 import hashlib
 import time
 import re
+import xbmcaddon
 import xbmcplugin
+inicio = time.time()
+__addon__ = xbmcaddon.Addon(str(sys.argv[0]))
+addon_dir = xbmc.translatePath(__addon__.getAddonInfo('path'))
+sys.path.append(os.path.join(addon_dir, 'resources', 'lib' ))
 from fuzzywuzzy import fuzz
 
-inicio = time.time()
+base_url = __addon__.getSetting("base_url")
+use_cache_tvshows_list = __addon__.getSetting("use_cache_tvshows_list")
+cache_tvshows_age = __addon__.getSetting("cache_tvshows_age") * 60 * 60
+use_cache_episodes_list = __addon__.getSetting("use_cache_episodes_list")
+cache_episodes_age = __addon__.getSetting("cache_episodes_age") * 60 * 60
+use_fuzzy = __addon__.getSetting("use_fuzzy")
+fuzzy_threshold = __addon__.getSetting("fuzzy_threshold")
 
 PREFIX_LOG = 'EZTV - '
-BASE_URL = "https://eztv.it"
-HEADERS = { 'Referer' : BASE_URL,
+HEADERS = { 'Referer' : base_url,
             'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'
 }
-
-cache_age = 12 * 60 * 60
-use_fuzzy = True
-# threshold recommended 90-97
-fuzzy_threshold = 92
-
+cache_prefix = xbmc.translatePath('special://temp') + __addon__.getAddonInfo('name').lower().replace(' ','_') + '_cache_'
+print cache_prefix
 def search(query):
     return []
 
@@ -31,14 +37,17 @@ def search_movie(imdb_id, name, year):
 def search_episode(imdb_id,tvdb_id,name,season,episode):
     show_list = get_eztv_shows()
     episode_string = '(?:S' + str(season).zfill(2) + 'E' + str(episode).zfill(2) + '|' + str(season) + 'x' + str(episode).zfill(2) + ')'
-    print PREFIX_LOG + 'Seaching for: ' + name + ' (' + str(season).zfill(2) + 'E' + str(episode).zfill(2) + ')'
+    print PREFIX_LOG + 'Seaching for: ' + name + ' (S' + str(season).zfill(2) + 'E' + str(episode).zfill(2) + ')'
     result = []
     show_found = ''
     for item in show_list:
         if ((name == item['name1']) | (name == item['name2']) | (name == item['name3']) | (name == item['name4'])):
-            url_show = BASE_URL + '/shows/' + item['id'] + '/'
-            req = urllib2.Request(url_show, headers=HEADERS)
-            data = urllib2.urlopen(req).read()
+            data = ''
+            url_show = base_url + '/shows/' + item['id'] + '/'
+            if(use_cache_episodes_list):
+                data = get_url(url_show, True)
+            else:
+                data = get_url(url_show, False)
             for magnet in re.findall(r'(magnet.*' + episode_string + '.*)" class="magnet"', data, re.IGNORECASE):
                 result.append({'uri': magnet})
             show_found = name
@@ -61,9 +70,12 @@ def search_episode(imdb_id,tvdb_id,name,season,episode):
             item = sorted_showlist[-1]
             if(item['score'] >= fuzzy_threshold ):
                 print PREFIX_LOG + 'Fuzzy found: ' + item['name'] + ' (score: ' + str(item['score']) + ')'
-                url_show = BASE_URL + '/shows/' + item['id'] + '/'
-                req = urllib2.Request(url_show, headers=HEADERS)
-                data = urllib2.urlopen(req).read()
+                url_show = base_url + '/shows/' + item['id'] + '/'
+                data = ''
+                if(use_cache_episodes_list):
+                    data = get_url(url_show, True)
+                else:
+                    data = get_url(url_show, False)
                 for magnet in re.findall(r'(magnet.*' + episode_string + '.*)" class="magnet"', data, re.IGNORECASE):
                     result.append({'uri': magnet})
             else:
@@ -74,7 +86,12 @@ def search_episode(imdb_id,tvdb_id,name,season,episode):
     return result
 
 def get_eztv_shows():
-    data = get_cached_url(BASE_URL + '/showlist/')
+    data = ''
+    url_show_list = base_url + '/showlist/'
+    if(use_cache_tvshows_list):
+        data = get_url(url_show_list, True)
+    else:
+        data = get_url(url_show_list, False)
     eztv_shows = []
     for show_id, show_named_id, show_name in re.findall(r'<a href="/shows/([0-9][0-9]*)/(.*)/" class="thread_link">(.*)</a></td>', data):
         name1 = re.sub('[-]', ' ', show_named_id)
@@ -102,27 +119,36 @@ def get_eztv_shows():
         })
     return eztv_shows
 
-def get_cached_url(url):
+def get_url(url,use_cache=False):
+    print PREFIX_LOG + 'Downloading ' + url
     data = ''
     m = hashlib.md5()
     m.update(url)
     url_hash = m.hexdigest()
-    cache_file = xbmc.translatePath('special://temp') + url_hash
-    print PREFIX_LOG + 'Cache -> ' + cache_file
-    if(os.path.isfile(cache_file)):
-        if ((time.time() - os.stat(cache_file).st_mtime)  > cache_age):
-            print PREFIX_LOG + 'Invalid cache!'
+    cache_file = cache_prefix + url_hash
+    if (use_cache):
+        print PREFIX_LOG + 'Cache -> ' + cache_file
+        if(os.path.isfile(cache_file)):
+            if ((time.time() - os.stat(cache_file).st_mtime)  > cache_tvshows_age):
+                print PREFIX_LOG + 'Invalid cache!'
+                req = urllib2.Request(url, headers=HEADERS)
+                data = urllib2.urlopen(req).read()
+                f = open(cache_file, "w")
+                f.write(data)
+                f.close()
+            else:
+                f = open(cache_file, "r")
+                data = f.read()
+                f.close()
+        else:
+            print PREFIX_LOG + 'No cache!'
             req = urllib2.Request(url, headers=HEADERS)
             data = urllib2.urlopen(req).read()
             f = open(cache_file, "w")
             f.write(data)
             f.close()
-        else:
-            f = open(cache_file, "r")
-            data = f.read()
-            f.close()
     else:
-        print PREFIX_LOG + 'No cache!'
+        print PREFIX_LOG + 'Dont use cache!!'
         req = urllib2.Request(url, headers=HEADERS)
         data = urllib2.urlopen(req).read()
         f = open(cache_file, "w")
